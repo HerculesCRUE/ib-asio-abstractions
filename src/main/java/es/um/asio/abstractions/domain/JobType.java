06b3032814d181fcfa732b9e992ce5ba5dbbec5a
Atorrecilla <atorrecilla@izertis.com>
2021-06-28 15:52:53 +0200

    COnstantes CERIF

--- src/main/java/es/um/asio/abstractions/domain/JobType.java
+++ src/main/java/es/um/asio/abstractions/domain/JobType.java
@@ -3,25 +3,29 @@ package es.um.asio.abstractions.domain;
 /**
  * The Enum that represents the job type
  */
-public enum JobType {    
+public enum JobType {
 
-    /**
-     * Unkown job type.
-     */
-    UNKNOWN, 
-    
-    /**
-     * Dataset job type.
-     */
-    DATASET, 
-    
-    /**
-     * Cvn job type.
-     */
-    CVN, 
-    /** 
-     * OAIPMH job type. 
-     */
-    OAIPMH,
-    
+	/**
+	 * Unkown job type.
+	 */
+	UNKNOWN,
+
+	/**
+	 * Dataset job type.
+	 */
+	DATASET,
+
+	/**
+	 * Cvn job type.
+	 */
+	CVN,
+	/**
+	 * OAIPMH job type.
+	 */
+	OAIPMH,
+	/**
+	 * CERIF job type.
+	 */
+	CERIF,
+
 }
